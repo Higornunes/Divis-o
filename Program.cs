679568065d6f50eefcadf3f99d515d4b117fea66
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 21:49:55 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double numerador, denominador, resultado;
+
+Console.Write("Digite o numerador.....: ");
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o denominador...: ");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+if (denominador != 0)
+{
+    resultado = numerador / denominador;
+    Console.WriteLine($"{numerador} dividido por {denominador} é {resultado}");
+}
+else
+{
+    Console.WriteLine("Não é possível dividir por zero.");
+}
